864f174b9bee6b333827df61a3dfffc1f188ebb5
annanovikova614 <72442702+annanovikova614@users.noreply.github.com>
2020-12-25 00:30:05 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите номер теста: ");
